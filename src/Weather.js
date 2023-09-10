b7243daaae57d194d0828ba5c64eeff9fca52ec8
RuRi-Code-Cloud <rudziter367@gmail.com>
2023-09-10 18:43:07 +0100

    Added axios to show current temp.

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import { ColorRing } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function showResponse(response) {
+    alert(
+      `Hello from ${response.data.name} it is ${response.data.main.temp} °C`
+    );
+  }
+
+  let key = `2513f3c728b1b5ff4f4347e1a6af22b8`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${key}&units=metric`;
+  axios.get(url).then(showResponse);
+  return (
+    <ColorRing
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="blocks-loading"
+      wrapperStyle={{}}
+      wrapperClass="blocks-wrapper"
+      colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+    />
+  );
+}
