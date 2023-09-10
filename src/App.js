73580966a554d150612c3bfbb2228396021353e0
RuRi-Code-Cloud <rudziter367@gmail.com>
2023-09-10 22:01:44 +0100

    fixed css properties

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
+import "./Weather.css";
+import "./First.css";
+import "./Second.css";
+import "./Third.css";
+import "./Source.css";
 import Weather from "./Weather";
+import First from "./First";
+import Second from "./Second";
+import Third from "./Third";
 import Source from "./Source";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello!</h1>
-        <Weather city="Cesis" />
+        <Weather />
+        <First />
+        <Second />
+        <Third />
         <Source className="Footer" />
       </header>
     </div>
